d88a75e7b61a422434b132a5076b0fcd8222db41
Aleksandar Stojanovic <31828736+AleksandarStojanovic91@users.noreply.github.com>
2020-07-15 01:35:48 +0200

    Add files via upload

---
+++ Vezba4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("from");
+        System.out.println("the");
+        System.out.println("Debug");
+        System.out.println("Mode");
+    }
+}
